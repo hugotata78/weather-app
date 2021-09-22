cfa677a876469af5119dfd7b910276bb6473d3da
hugotata78 <hugotatarinoff@gmail.com>
2021-09-22 16:49:38 -0300

    primer commit

---
+++ front/src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div class="card">
+
+            <div class="top-row background-top-row">
+                <h4>Twitter</h4>
+            </div>
+
+            <div class="content">
+                <h2>Twitter</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odit repellat quo placeat illo officia, assumenda repudiandae commodi dolor libero.</p>
+                <button class="button background-top-row">Empezar</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card
