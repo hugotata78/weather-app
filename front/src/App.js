f8ab7eefe48c97ad66c9c9140f290c5e355c244b
hugotata78 <hugotatarinoff@gmail.com>
2021-09-22 18:23:52 -0300

    mejoras

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurrentWeatherData } from './redux/actions';
+import { useCallback, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
 
 function App() {
+
+  const dispatch = useDispatch()
+  const current_data = useSelector(state => state.weatherReducers.current_data)
+
+  const getLocation = useCallback((position) => {
+    const { latitude, longitude } = position.coords
+    dispatch(getCurrentWeatherData(latitude, longitude))
+  }, [dispatch])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(getLocation)
+  }, [getLocation])
+
   return (
-    <div class="container">
-      <Card />
+    <div>
+      <SearchBar/>
+      <div class="container">
+        {current_data && <Card data={current_data} />}
+      </div>
     </div>
   );
 }
--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ data }) => {
+
+    console.log(data)
     return (
         <div class="card">
 
             <div class="top-row background-top-row">
-                <h4>Twitter</h4>
+                <h4>{data.name}</h4>
             </div>
 
             <div class="content">
-                <h2>Twitter</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odit repellat quo placeat illo officia, assumenda repudiandae commodi dolor libero.</p>
-                <button class="button background-top-row">Empezar</button>
+                <h2>Data</h2>
+                <p>Temp: {data.main.temp}</p>
+                <p>Pressure: {data.main.pressure}</p>
+                <p>Humidity: {data.main.humidity}</p>
+                <p>Visibility: {data.visibility}</p>
+                <p>Wind: speed {data.wind.speed}</p>
             </div>
 
         </div>
